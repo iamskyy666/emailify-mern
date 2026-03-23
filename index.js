a2c7b3bd81871d9b72e8c57600fd0ac33e23d30b
Soumadip <39thdecember@gmail.com>
2026-03-23 17:41:22 +0530

    Refactor Server-Structure ✅

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-import dotenv from "dotenv";
-dotenv.config();
-
+import "./config/dotenv.js"
 import express from "express";
-import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-// import { googleClientID, googleClientSecret } from "./config/keys.js";
+import "./services/passport.services.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-// middlewares
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(`🟡 accessToken: ${accessToken}`);
-      console.log(`🔵 refreshToken: ${refreshToken}`);
-      console.log("👤 profile:", profile);
-      //console.log(`✅ done: ${done}`);
-    },
-  ),
-); // instance of Google password-strategy
+//routes
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
-
-// OAuth-flow
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  }),
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(PORT, () => {
   console.log(`App running on PORT: ${PORT} ☑️`);
---
+++ services/passport.services.js
@@ -0,0 +1,19 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+// import { googleClientID, googleClientSecret } from "../config/keys";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(`🟡 accessToken: ${accessToken}`);
+      console.log(`🔵 refreshToken: ${refreshToken}`);
+      console.log("👤 profile:", profile);
+      //console.log(`✅ done: ${done}`);
+    },
+  ),
+); // instance of Google password-strategy
