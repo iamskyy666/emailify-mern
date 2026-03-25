82f2a5168e4d97eb8bb66399fee5fd7bafac5929
Soumadip <39thdecember@gmail.com>
2026-03-26 04:39:15 +0530

    Enable Cookies 🍪

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ import "./models/user.model.js";
 import "./services/passport.services.js";
 import authRoutes from "./routes/auth.routes.js";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import passport from "passport";
 
 mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
+
+// use-cookies (cookie-session)
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    keys: [process.env.COOKIE_KEY],
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //routes
 authRoutes(app);
